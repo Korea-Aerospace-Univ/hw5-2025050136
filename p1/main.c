5159799832fd18e8224018627ff4e0a6c07d5fe4
2025050136 <rlorv1213@kau.kr>
2026-04-29 15:27:42 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -4,7 +4,7 @@ int main() {
     int N;
     scanf("%d", &N);
 
-    int found=0;
+    int possible=0;
 
     for (int i = 1 ; i<= (N/900) ; i++) {
         for (int j = 2 ; j <= (N/750) ; j+=2) {
@@ -13,7 +13,7 @@ int main() {
                 if (900 * i + 750 * j + 200 * k == N) {
                     if (k < i || k < j) {
                         printf("%d %d %d\n", i, j, k);
-                        found = 1;
+                        possible=1;
                     }
                 }
 
@@ -21,7 +21,7 @@ int main() {
         }
     }
 
-    if (!found) {
+    if (!possible) {
         printf("none");
     }
 
